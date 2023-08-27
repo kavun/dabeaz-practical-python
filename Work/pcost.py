382bc795d6f67563900186e4060f9ee7904c7dfd
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-08-27 16:59:44 -0500

    3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
-import csv
+from report import read_portfolio_2_5
+
 
 def pcost(filename):
-    '''Returns the total cost of the portfolio'''
-    with open(filename, 'rt') as f:
-        lines = csv.reader(f)
-        cost = 0.0
-        header_line = next(lines)
-        print(header_line)
-        for line_no, line in enumerate(lines):
-            record = dict(zip(header_line, line))
-            try:
-                shares = int(record['shares'])
-                price = float(record['price'])
-                cost += shares * price
-            except ValueError:
-                print(f'Could not parse line {line_no} with data: {line}')
-    return cost
+    """Returns the total cost of the portfolio"""
+    portfolio = read_portfolio_2_5(filename)
+    return sum(map(lambda p: p["shares"] * p["price"], portfolio))
